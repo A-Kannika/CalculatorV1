f3c09834d6a5dd353016b1b52eb7c42453e87797
Kannika Armstrong <49578771+A-Kannika@users.noreply.github.com>
2021-07-20 17:36:16 -0700

    Update NumericCalculator.java

--- src/NumericCalculator.java
+++ src/NumericCalculator.java
@@ -4,6 +4,7 @@
  * Programming assignment: Develop Multi-Function Calculator Version 1
  * Numerical Calculator
  * Professor. Kivanç A. Dincer, Ph.D.
+ * The University of Washington Tacoma
  */
 
 import java.util.Scanner;
