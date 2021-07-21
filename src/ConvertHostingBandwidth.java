110569de0aea6ab2997cd574074bfbc7ff7822ae
Kannika Armstrong <49578771+A-Kannika@users.noreply.github.com>
2021-07-20 17:35:55 -0700

    Update ConvertHostingBandwidth.java

--- src/ConvertHostingBandwidth.java
+++ src/ConvertHostingBandwidth.java
@@ -4,6 +4,7 @@
  * Programming assignment: Develop Multi-Function Calculator Version 1
  * Convert Hosting Bandwidth
  * Professor. Kivanç A. Dincer, Ph.D.
+ * The University of Washington Tacoma
  */
 
 import java.math.BigDecimal;
