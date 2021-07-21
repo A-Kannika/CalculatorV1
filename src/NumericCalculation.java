c5e15686dabdcbab2480ec403a88c173d88eacb9
Kannika Armstrong <49578771+A-Kannika@users.noreply.github.com>
2021-07-20 17:36:07 -0700

    Update NumericCalculation.java

--- src/NumericCalculation.java
+++ src/NumericCalculation.java
@@ -4,6 +4,7 @@
  * Programming assignment: Develop Multi-Function Calculator Version 1
  * Numerical Calculation
  * Professor. Kivanç A. Dincer, Ph.D.
+ * The University of Washington Tacoma
  */
 
 public class NumericCalculation extends NumericCalculator {
