24974ad03ef62d76d5affac3727a777a2e4eeb02
Kannika Armstrong <49578771+A-Kannika@users.noreply.github.com>
2021-07-20 17:35:24 -0700

    Update ConvertDataUnits.java

--- src/ConvertDataUnits.java
+++ src/ConvertDataUnits.java
@@ -4,6 +4,7 @@
  * Programming assignment: Develop Multi-Function Calculator Version 1
  * Convert Data Unit
  * Professor. Kivanç A. Dincer, Ph.D.
+ * The University of Washington Tacoma
  */
 
 import java.math.BigDecimal;
