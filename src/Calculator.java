ca8e39133bb6f455e0a0147a6b27e5848c9d1b05
Kannika Armstrong <49578771+A-Kannika@users.noreply.github.com>
2021-07-20 17:34:40 -0700

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,6 +4,7 @@
  * Programming assignment: Develop Multi-Function Calculator Version 1
  * Calculate main class
  * Professor. Kivanç A. Dincer, Ph.D.
+ * The University of Washington Tacoma
  */
 
 import java.util.Scanner;
